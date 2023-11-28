3520c8e1833b7822f42dd0ca3b56e98bf2723499
wonjinKim <wonjin.kim0911@gmail.com>
2023-11-28 13:00:30 +0900

    멤버 셀러 VO 추가 - 원진

--- FTD_Project/src/main/java/com/kh/ftd/seller/model/vo/Seller.java
+++ FTD_Project/src/main/java/com/kh/ftd/seller/model/vo/Seller.java
@@ -2,5 +2,21 @@ package com.kh.ftd.seller.model.vo;
 
 
 public class Seller {
-
+	
+	private int sellerNo;		//	SELLER_NO	NUMBER
+	private String companyName;	//	COMPANY_NAME	VARCHAR2(100 BYTE)
+	private String sellerId;	//	SELLER_ID	VARCHAR2(25 BYTE)
+	private String sellerPwd;	//	SELLER_PWD	VARCHAR2(100 BYTE)
+	private String sellerName;	//	SELLER_NAME	VARCHAR2(15 BYTE)
+	private String companyNo;	//	COMPANY_NO	VARCHAR2(12 BYTE)
+	private String businessNo;	//	BUSINESS_NO	VARCHAR2(30 BYTE)
+	private String address;		//	ADDRESS	VARCHAR2(30 BYTE)
+	private String email;		//	EMAIL	VARCHAR2(40 BYTE)
+	private String phone;		//	PHONE	VARCHAR2(13 BYTE)
+	private String accountNo;	//	ACCOUNT_NO	VARCHAR2(30 BYTE)
+	private String deleteDate;	//	DELETE_DATE	DATE
+	private String enrollDate;	//	ENROLL_DATE	DATE
+	private String acceptDate;	//	ACCEPT_DATE	DATE
+	private String refuseReason;//	REFUSE_REASON	VARCHAR2(30 BYTE)
+	
 }
