0b766e6bf9711033a0288d4fe7e44cb109cb51f6
jeonhj1015 <150875181+jeonhj1015@users.noreply.github.com>
2023-11-28 14:00:01 +0900

    feat : 공지사항 service, vo 작
    feat : 공지사항 페이지 구현시 필요한 service 와 vo 를 작성

--- FTD_Project/src/main/java/com/kh/ftd/notice/model/vo/Notice.java
+++ FTD_Project/src/main/java/com/kh/ftd/notice/model/vo/Notice.java
@@ -1,5 +1,25 @@
 package com.kh.ftd.notice.model.vo;
 
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Notice {
 
+	private int noticeNo; //NOTICE_NO	NUMBER
+	private String noticeTitle;//NOTICE_TITLE	VARCHAR2(400 BYTE)
+	private String noticeContent;//NOTICE_CONTENT	VARCHAR2(3000 BYTE)
+	private int count;//COUNT	NUMBER
+	private Date createDate;//CREATE_DATE	DATE
+	private Date deleteDate;//DELETE_DATE	DATE
+	
 }
