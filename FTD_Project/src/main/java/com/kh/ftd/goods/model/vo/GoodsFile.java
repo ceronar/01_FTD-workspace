13a4b00425918cb7f2e42f7a20b28469a7bbc60d
Genius0q <genius0q@gmail.com>
2023-11-29 13:03:07 +0900

    feat : vo 생성
    feat : vo 생성

--- FTD_Project/src/main/java/com/kh/ftd/goods/model/vo/GoodsFile.java
+++ FTD_Project/src/main/java/com/kh/ftd/goods/model/vo/GoodsFile.java
@@ -1,5 +1,20 @@
 package com.kh.ftd.goods.model.vo;
 
-public class GoodsFile {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class GoodsFile {
+	
+	private int fileNo;				//	FILE_NO	NUMBER
+	private int sellNo;				//	SELL_NO	NUMBER
+	private String originalName; 	//	ORIGINAL_NAME	VARCHAR2(300 BYTE)
+	private String changeName; 		//	CHANGE_NAME	VARCHAR2(300 BYTE)
+	private String createDate; 		//	CREATE_DATE	DATE
+	private String deleteDate;		//	DELETE_DATE	DATE
 }
